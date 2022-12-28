faed4f7e4a9abdf5053d0ce3772e9ccd9c6f1879
Bhaskar Dhruv Kuncha <68691564+dhruvkuncha@users.noreply.github.com>
2022-12-28 12:22:49 +0100

    modal

--- app.js
+++ app.js
@@ -93,6 +93,20 @@ app.get("/pricing", (req, res) => {
     });
 });
 
+app.get("/barber", (req, res) => {
+  const sessionCookie = req.cookies.session || "";
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(async (userData) => {
+      // console.log("Logged in:", userData);
+      res.render("barber", { loggedIn: true });
+    })
+    .catch((error) => {
+      res.render("barber", { loggedIn: false });
+    });
+});
+
 // app.get("/gallery", (req, res) => {
 //   // res.sendFile(__dirname + "/views/gallery.ejs");
 //   res.render('gallery');
